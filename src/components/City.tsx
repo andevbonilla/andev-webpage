3c0aff1826d67361f53038fdcfc28eac6c04248e
Andres Bonilla <andresbonilla1256@gmail.com>
2024-07-06 14:44:15 -0500

    small change to redeploy

--- src/components/City.tsx
+++ src/components/City.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 
 export const City = () => {
   return (
-        <div className='city scale-150 lg:scale-100 absolute bottom-[6rem] md:bottom-[8rem] left-0 w-full' id="part1">
-            <Image 
-              src={require("@/assets/city.png")}
-              alt="singapur city"
-              className='w-full scale-150 -translate-x-6'
-            />
-        </div>
+    <div className='city scale-150 lg:scale-100 absolute bottom-[6rem] md:bottom-[8rem] left-0 w-full' id="part1">
+      <Image
+        src={require("@/assets/city.png")}
+        alt="Singapur city"
+        className='w-full scale-150 -translate-x-6'
+      />
+    </div>
   )
 }
