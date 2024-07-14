e85fb07f55de250d1ca3a79fcd4c009feb8933c2
Andres Bonilla <andresbonilla1256@gmail.com>
2024-07-13 22:40:14 -0500

    change the city again

--- src/components/City.tsx
+++ src/components/City.tsx
@@ -5,9 +5,14 @@ export const City = () => {
   return (
     <div className='w-full' id="part1">
       <Image
-        src={require("@/assets/posible_town.png")}
-        alt="okokok"
-        className='w-full'
+        src={require("@/assets/posible_town2.webp")}
+        alt="greek town in andev page"
+        className='hidden md:flex w-full'
+      />
+      <Image
+        src={require("@/assets/posible_town_mobile.webp")}
+        alt="greek town in andev page"
+        className='md:hidden w-full'
       />
     </div>
   )
