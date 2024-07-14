5e49bde5485a4cfb7439b737ebfcc328c95046a6
Andres Bonilla <andresbonilla1256@gmail.com>
2024-07-13 22:12:22 -0500

    change the city image

--- src/components/City.tsx
+++ src/components/City.tsx
@@ -3,26 +3,11 @@ import React from 'react';
 
 export const City = () => {
   return (
-    <div className='relative' id="part1">
+    <div className='w-full' id="part1">
       <Image
-        src={require("@/assets/layers-forest/layer1-new.png")}
+        src={require("@/assets/posible_town.png")}
         alt="okokok"
-        className='w-full absolute top-[-10rem]'
-      />
-      <Image
-        src={require("@/assets/layers-forest/layer2-new.png")}
-        alt="okokok"
-        className='w-full absolute top-[-10rem]'
-      />
-      <Image
-        src={require("@/assets/layers-forest/layer3-new.png")}
-        alt="okokok"
-        className='w-full absolute top-[-10rem]'
-      />
-      <Image
-        src={require("@/assets/layers-forest/layer4-new.png")}
-        alt="okokok"
-        className='w-full absolute top-[-10rem]'
+        className='w-full'
       />
     </div>
   )
