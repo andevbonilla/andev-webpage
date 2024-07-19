32bc1b71fe999eba6d2db578e3fc893623a29b9a
Andres Bonilla <andresbonilla1256@gmail.com>
2024-07-19 01:56:48 -0500

    piques almost finished in the page

--- src/components/City.tsx
+++ src/components/City.tsx
@@ -1,22 +1,28 @@
 "use client"
-import { Press_Start_2P } from 'next/font/google';
 import Image from 'next/image';
-import React, { useEffect, useRef } from 'react';
-import { Cars } from './ui/Cars';
+import React, { useEffect, useRef, useState } from 'react';
+import { RaceTrack } from './ui/Cars';
+import { Indie_Flower } from "next/font/google";
 
-const orbitron = Press_Start_2P({
+const indieFlower = Indie_Flower({
   weight: ["400"],
   style: ["normal"],
   subsets: ["latin"],
-})
-
+});
 
 export const City = () => {
-
   const sun: any = useRef(null);
+  const [gt3rsClassList, setGt3rsClassList] = useState("static-gt3rs");
+  const [brabusClassList, setBrabusClassList] = useState("static-brabus");
+  const [bugattiClassList, setBugattiClassList] = useState("static-bugatti");
+  const [sanderoClassList, setSanderoClassList] = useState("static-sandero");
+  const [x6mClassList, setX6mClassList] = useState("static-x6m");
+  const [ducatiClassList, setDucatiClassList] = useState("static-ducati");
+
+  // Estado adicional para forzar el reinicio
+  const [raceKey, setRaceKey] = useState(0);
 
   useEffect(() => {
-
     let initialSunPosition = -400;
     let beforeScroollPosition = 0;
     let actualScroollPosition = 0;
@@ -27,95 +33,81 @@ export const City = () => {
     }
 
     const handleScroll = () => {
-
-      // ig go down -> +1
-
-
       if (sun.current) {
-
         sunPosition = sun.current.getBoundingClientRect();
 
         if (sunPosition.top <= 400 && sunPosition.top >= -500) {
-
           actualScroollPosition = window.scrollY;
 
           if (actualScroollPosition >= beforeScroollPosition) {
-
             if (initialSunPosition + 2 < 0) {
               initialSunPosition = initialSunPosition + 2;
             }
-
-
           } else {
-
             if (initialSunPosition - 2 >= -400) {
               initialSunPosition = initialSunPosition - 2;
             }
-
           }
 
           beforeScroollPosition = actualScroollPosition;
-
           sun.current.style.top = `${initialSunPosition}px`; // Ajustar posición del sol
-
-        };
-
-      };
-
+        }
+      }
     };
 
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-
   }, []);
 
+  const restartRace = () => {
+    setGt3rsClassList("gt3rs");
+    setBrabusClassList("brabus900");
+    setBugattiClassList("bugatti");
+    setSanderoClassList("sandero");
+    setX6mClassList("x6m");
+    setDucatiClassList("ducati");
+
+    // Forzar el reinicio de la animación
+    setRaceKey(prevKey => prevKey + 1);
+  };
 
   return (
     <div className='w-full relative mt-10'>
       <div ref={sun} className='lg:w-[25rem] lg:h-[25rem] absolute left-[15%] w-[18rem] h-[18rem] bg-yellow-400 rounded-full z-10 sun'></div>
 
-      <div className='hidden md:flex flex-col items-center justify-center w-full'>
-        <Image
-          src={require("@/assets/monaco.webp")}
-          alt="greek town in andev page"
-          className='flex w-full relative z-20'
-        />
-        <Cars />
-      </div>
+      <Image
+        alt='city'
+        src={require("@/assets/monaco.webp")}
+        className='w-full z-10'
+      />
+      <RaceTrack
+        key={raceKey} // Usar la clave para forzar el re-render
+        gt3rsClassList={gt3rsClassList}
+        brabusClassList={brabusClassList}
+        bugattiClassList={bugattiClassList}
+        sanderoClassList={sanderoClassList}
+        x6mClassList={x6mClassList}
+        ducatiClassList={ducatiClassList}
+      />
 
-      <div className='md:hidden flex flex-col items-center justify-center w-full'>
-
-        <Image
-          src={require("@/assets/monaco-mobile.webp")}
-          alt="greek town in andev page"
-          className='flex w-full relative z-20'
-        />
-        <Cars />
-
-      </div>
 
       {/* the traffic signal */}
       <div className='w-full flex items-end justify-start absolute bottom-0 left-0 z-30'>
         <div className='flex flex-col items-center ml-[2rem] lg:ml-[5rem]'>
-          <div className='bg-yellow-500 h-[13rem] w-[8rem] lg:h-[14rem] lg:w-[10rem] rounded-md flex justify-center items-center'>
-            <div className='bg-yellow-500 border-[.4rem] border-black rounded-md w-[94%] h-[94%] p-4 flex flex-col justify-between items-center'>
-              <div className='flex flex-col items-center justify-center mb-4'>
-                <h4 className='font-bold text-black'>Your</h4>
-                <h5 className='font-bold text-black'>Speed</h5>
-                <p className='font-bold text-black'>km/h</p>
-              </div>
-              <div className='bg-black bg-opacity-90 flex items-center justify-center rounded-md mb-2'>
-                <p className={`text-red-600 p-4 font-bold text-2xl lg:text-3xl ${orbitron.className}`}>090</p>
+          <div className='bg-yellow-500 rounded-md flex justify-center items-center'>
+            <div className='bg-yellow-500 border-[.4rem] border-black rounded-md w-[94%] h-[94%] m-1 p-4 flex flex-col justify-between items-center'>
+              <p className={`text-white text-center font-bold text-2xl lg:text-3xl ${indieFlower.className}`}>Click me!</p>
+              <div
+                onClick={restartRace}
+                className='bg-red-600 w-[3.5rem] h-[3.5rem] rounded-full cursor-pointer shadow-lg flex items-center justify-center hover:bg-red-700 active:bg-red-700 transition duration-200'>
               </div>
             </div>
           </div>
           <div className='lg:h-[20rem] h-[10rem] w-[.8rem] bg-gray-400'></div>
         </div>
       </div>
-
     </div>
-
-  )
+  );
 }
--- src/components/ui/Cars.tsx
+++ src/components/ui/Cars.tsx
@@ -1,46 +1,52 @@
-import Image from 'next/image'
-import React from 'react'
+"use client"
+import Image from 'next/image';
 
-export const Cars = () => {
+export const RaceTrack = ({
+    gt3rsClassList,
+    brabusClassList,
+    bugattiClassList,
+    sanderoClassList,
+    x6mClassList,
+    ducatiClassList
+}: any) => {
+
+
     return (
-        < div className='flex w-full relative z-20' >
+        <div className='flex w-full h-[12rem] relative z-30 scenary'>
+
             <Image
-                src={require("@/assets/scenary.webp")}
-                alt="greek town in andev page"
-                className='flex w-full z-20'
+                src={require("@/assets/car-brabus900.png")}
+                alt="Brabus Rocket 900"
+                className={`absolute w-[10.8rem] ${brabusClassList} bottom-[70%] xl:bottom-[60%] right-0`}
             />
-            <div className='absolute w-full top-0 left-0 right-0 z-30'>
-                <Image
-                    src={require("@/assets/car-brabus900.png")}
-                    alt="Brabus Rocket 900"
-                    className='absolute w-[18rem] brabus900'
-                />
-                <Image
-                    src={require("@/assets/car-bugatti.png")}
-                    alt="Buguatti Chiron"
-                    className='absolute w-[17rem] bugatti'
-                />
-                <Image
-                    src={require("@/assets/car-gt3rs.png")}
-                    alt="Porsche 911 GT3 rs"
-                    className='absolute w-[16rem] gt3rs'
-                />
-                <Image
-                    src={require("@/assets/car-sandero.png")}
-                    alt="Renault Sandero"
-                    className='absolute w-[16rem] sandero'
-                />
-                <Image
-                    src={require("@/assets/car-x6m.png")}
-                    alt="Bmw X6M"
-                    className='absolute w-[21rem] x6m'
-                />
-                <Image
-                    src={require("@/assets/bike-ducati.png")}
-                    alt="Ducati streetfigther"
-                    className='absolute w-[11rem] ducati'
-                />
-            </div>
-        </div >
-    )
-}
+            <Image
+                src={require("@/assets/car-x6m.png")}
+                alt="BMW X6M"
+                className={`absolute w-[12.6rem] ${x6mClassList} bottom-[60%] xl:bottom-[50%] right-0`}
+            />
+            <Image
+                src={require("@/assets/car-sandero.png")}
+                alt="Renault Sandero"
+                className={`absolute w-[9.6rem] ${sanderoClassList} bottom-[55%] xl:bottom-[45%] right-0`}
+            />
+            <Image
+                src={require("@/assets/car-bugatti.png")}
+                alt="Bugatti Chiron"
+                className={`absolute w-[10.2rem] ${bugattiClassList} bottom-[50%] xl:bottom-[40%] right-0`}
+            />
+            <Image
+                src={require("@/assets/car-gt3rs.png")}
+                alt="Porsche 911 GT3 rs"
+                className={`absolute w-[9.6rem] ${gt3rsClassList} bottom-[45%] xl:bottom-[35%] right-0`}
+            />
+            <Image
+                src={require("@/assets/bike-ducati.png")}
+                alt="Ducati Streetfighter"
+                className={`absolute w-[6.6rem] ${ducatiClassList} bottom-[35%] xl:bottom-[25%] right-0`}
+            />
+
+        </div>
+    );
+};
+
+
